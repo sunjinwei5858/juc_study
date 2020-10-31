a28c99b6b75adabae7c8aa9f4fdb25308554857b
sunjinwei <sunjinwei5828@163.com>
2020-10-31 12:12:00 +0800

    jvm类加载机制的坑，static方法和字段，被调用，就会初始化。如果字段被static还加了final，那么就不会初始化这个类

---
+++ src/main/java/com/sunjinwei/load/Demo01.java
@@ -0,0 +1,46 @@
+package com.sunjinwei.load;
+
+
+/**
+ * java的类加载机制学习：【主要是深入理解java虚拟机这本书的demo】
+ * 场景1：子类调用父类的static字段，自己本身不会初始化，但是父类会初始化。因为java虚拟机规范 对于静态字段，只有定义这个字段的类才会被初始化。子类只是调用父类，只会触发父类的初始化。
+ * 场景2：子类调用父类的final+static字段，都不会初始化，因为被final修饰，已经被添加到常量池了。
+ */
+public class Demo01 {
+
+    public static void main(String[] args) {
+        // 测试static
+        //int num = SubChild.num;
+
+        //测试final+static
+        //int result = SubChild.result;
+    }
+
+}
+
+
+class Parent {
+
+    static {
+        System.out.println("父类初始化");
+    }
+
+    /**
+     * only static
+     */
+    public static int num = 100;
+
+    /**
+     * static+final 【有坑】
+     */
+    public static final int result = 100;
+
+}
+
+class SubChild extends Parent {
+
+    static {
+        System.out.println("子类初始化");
+    }
+
+}
