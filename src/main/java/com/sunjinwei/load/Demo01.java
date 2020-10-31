3156200ada317cf604bec71cd3f95234bd5adf79
sunjinwei <sunjinwei5828@163.com>
2020-10-31 12:19:47 +0800

    解释为什么父类的静态常量被调用方引用，也不会实例化这个类，因为这个常量在【编译阶段】放在了调用方的常量池了【出自深入理解jvm】

--- src/main/java/com/sunjinwei/load/Demo01.java
+++ src/main/java/com/sunjinwei/load/Demo01.java
@@ -4,7 +4,7 @@ package com.sunjinwei.load;
 /**
  * java的类加载机制学习：【主要是深入理解java虚拟机这本书的demo】
  * 场景1：子类调用父类的static字段，自己本身不会初始化，但是父类会初始化。因为java虚拟机规范 对于静态字段，只有定义这个字段的类才会被初始化。子类只是调用父类，只会触发父类的初始化。
- * 场景2：子类调用父类的final+static字段，都不会初始化，因为被final修饰，已经被添加到常量池了。
+ * 场景2：子类调用父类的final+static字段，都不会初始化，因为被final修饰，在编译阶段相当于这个常量放在了Demo01调用类的常量池了!!!
  */
 public class Demo01 {
 
@@ -31,7 +31,7 @@ class Parent {
     public static int num = 100;
 
     /**
-     * static+final 【有坑】
+     * static+final 【有坑】：被final修饰属于常量了，常量在编译阶段会存入调用类比如Demo01的常量池中，本质上没有直接引用到定义常量的类，因此不会触发到定义常量的类的初始化
      */
     public static final int result = 100;
 
